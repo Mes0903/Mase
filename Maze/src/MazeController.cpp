6dbd1b38c471ddc1e6341e0f318c93959e7dbf0b
Mes <mes900903@gmail.com>
2024-09-27 23:29:12 +0800

    construct the unique_ptr from raw pointer

--- Maze/src/MazeController.cpp
+++ Maze/src/MazeController.cpp
@@ -7,8 +7,8 @@
 
 void MazeController::init(MazeModel *model_ptr, MazeView *view_ptr)
 {
-  this->model_ptr = std::make_unique<MazeModel>(model_ptr);
-  this->view_ptr = std::make_unique<MazeView>(view_ptr);
+  this->model_ptr = std::unique_ptr<MazeModel>(model_ptr);
+  this->view_ptr = std::unique_ptr<MazeView>(view_ptr);
 }
 
 void MazeController::handleInput()
@@ -16,35 +16,29 @@ void MazeController::handleInput()
   // This method will be called in the main loop to handle user input
   // You can add ImGui::IsItemClicked() checks here to handle button clicks
   if (ImGui::IsItemClicked(0)) {    // 0 for left mouse button
-    if (ImGui::GetActiveID() == ImGui::GetID("Generate Maze (DFS)")) {
-      model_ptr->generateMazeDFS();
-    }
-    else if (ImGui::GetActiveID() == ImGui::GetID("Generate Maze (Prim's)")) {
-      model_ptr->generateMazePrim();
+    if (ImGui::GetActiveID() == ImGui::GetID("Generate Maze (Prim's)")) {
+      model_ptr->generateMazePrim(0);
     }
     else if (ImGui::GetActiveID() == ImGui::GetID("Generate Maze (Recursion)")) {
       model_ptr->generateMazeRecursion();
     }
     else if (ImGui::GetActiveID() == ImGui::GetID("Generate Maze (Recursion Division)")) {
-      model_ptr->generateMazeRecursionDivision();
+      model_ptr->generateMazeRecursionDivision(1, 1, MAZE_HEIGHT - 2, MAZE_WIDTH - 2);
     }
     else if (ImGui::GetActiveID() == ImGui::GetID("Solve Maze (DFS)")) {
-      model_ptr->solveMazeDFS();
+      model_ptr->solveMazeDFS(1, 0);
     }
     else if (ImGui::GetActiveID() == ImGui::GetID("Solve Maze (BFS)")) {
       model_ptr->solveMazeBFS();
     }
     else if (ImGui::GetActiveID() == ImGui::GetID("Solve Maze (UCS)")) {
-      model_ptr->solveMazeUCS();
+      model_ptr->solveMazeUCS(0);
     }
     else if (ImGui::GetActiveID() == ImGui::GetID("Solve Maze (Greedy)")) {
       model_ptr->solveMazeGreedy();
     }
-    else if (ImGui::GetActiveID() == ImGui::GetID("Solve Maze (Dijkstra)")) {
-      model_ptr->solveMazeDijkstra();
-    }
     else if (ImGui::GetActiveID() == ImGui::GetID("Solve Maze (A*)")) {
-      model_ptr->solveMazeAStar();
+      model_ptr->solveMazeAStar(0);
     }
   }
 }
